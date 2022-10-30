91155e42d41dd9b754825fade7a2292bb7def5a4
artem <artem-orlov92@bk.ru>
2022-10-30 20:03:06 +0300

    Initial Commit

---
+++ WhoWantToBeMillionaireChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhoWantToBeMillionaireChallenge
+//
+//  Created by Артем Орлов on 30.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
